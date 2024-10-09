b2095f6b08a7051cc14a9cf1b4aaf5ee1d496a64
ChielokaCode <realmatec01@gmail.com>
2024-10-09 17:43:33 +0100

    feat: updated verifyProduct fieldNames

--- src/Components/VerifyProduct.jsx
+++ src/Components/VerifyProduct.jsx
@@ -25,34 +25,34 @@ const VerifyProduct = ({ code }) => {
     if (result.data) {
       console.log(result.data);
       const [
-        productName,
-        productNafdacNo,
-        productForm,
-        dosageStrength,
-        batchNumber,
-        manufactureDate,
-        expirationDate,
-        manufacturerName,
-        manufacturerAddress,
-        storingCondition,
-        activeIngredients,
-        productImage,
+        ProductName,
+        ProductNafdacNo,
+        ProductForm,
+        DosageStrength,
+        BatchNumber,
+        ManufactureDate,
+        ExpirationDate,
+        ManufacturerName,
+        ManufacturerAddress,
+        StoringCondition,
+        ActiveIngredients,
+        ProductImage,
       ] = result.data; // Destructure the array returned from the contract
 
       // Map it to your productInfo structure
       setProductInfo({
-        productName,
-        productNafdacNo,
-        productForm,
-        dosageStrength,
-        batchNumber,
-        manufactureDate,
-        expirationDate,
-        manufacturerName,
-        manufacturerAddress,
-        storingCondition,
-        activeIngredients,
-        productImage,
+        ProductName,
+        ProductNafdacNo,
+        ProductForm,
+        DosageStrength,
+        BatchNumber,
+        ManufactureDate,
+        ExpirationDate,
+        ManufacturerName,
+        ManufacturerAddress,
+        StoringCondition,
+        ActiveIngredients,
+        ProductImage,
       });
 
       toast.success("Packet Information fetched successfully!");
@@ -93,7 +93,20 @@ const VerifyProduct = ({ code }) => {
           </table>
         </div>
       ) : (
-        <div>Product not Found</div>
+        <>
+          <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div className="text-center">
+              <p className="text-base font-semibold text-indigo-600">404</p>
+              <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                Product not found
+              </h1>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                Sorry, we couldn’t find the product you’re looking for.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6"></div>
+            </div>
+          </main>
+        </>
       )}
     </div>
   );
